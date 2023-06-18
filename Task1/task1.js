5e272e62eb87980985095343ead7a5450c591e91
Maxus94 <kmaxusk@gmail.com>
2023-06-18 17:26:42 +0300

    initial

---
+++ Task1/task1.js
@@ -0,0 +1,10 @@
+const categories = document.querySelectorAll(".item");
+console.log('Number of categories: ', categories.length);
+
+categories.forEach((categorie, index) => {
+  const subCatTitle = categorie.querySelector("h2");
+  const subCat = categorie.querySelectorAll("li");
+  console.log('Category: ', subCatTitle.innerText);
+  //console.log('Category: ', subCatTitle.textContent);
+  console.log('Elements: ', subCat.length);
+});
